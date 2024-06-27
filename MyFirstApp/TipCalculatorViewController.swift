d4d9a6c3860e18416061d81b6a328ddcc8638790
Austin <lyvuaustin@gmail.com>
2024-06-26 23:36:27 -0500

    Added current files

---
+++ MyFirstApp/TipCalculatorViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TipCalculatorViewController.swift
+//  MyFirstApp
+//
+//  Created by Austin Ly-Vu on 6/12/24.
+//
+
+import SwiftUI
+
+struct TipCalculatorViewController: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TipCalculatorViewController()
+}
--- MyFirstApp/ViewController.swift
+++ MyFirstApp/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var AppTitle: UILabel!
+    
+    
+    @IBAction func toTipCalculator(_ sender: UIButton) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        // Create a UIImageView
+        let backgroundImage = UIImageView(frame: UIScreen.main.bounds)
+
+        // Set the image from assets
+        backgroundImage.image = UIImage(named: "app_background")
+
+        // Set content mode to scale aspect fill
+        backgroundImage.contentMode = .scaleAspectFill
+
+        // Insert image view at the bottom of the view hierarchy
+        self.view.insertSubview(backgroundImage, at: 0)
     }
-
-
 }
 
